f20c7b9150209a2100beb4ca2d2e2def4abc8165
Mumyung <eoalsrud@naver.com>
2024-02-18 02:11:47 +0900

    한글 패치

---
+++ Source/VirtualConcert_Stage/UI/MainWidget.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUALCONCERT_STAGE_API UMainWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+
+public:
+
+
+	// ------------------------ Variable -----------------------
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category = "Animation", Transient, meta = (BindWidgetAnim) )
+	class UWidgetAnimation* Guide;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite , meta= (BindWidget), Category = "UI")
+	class UTextBlock* Text;
+
+	FWidgetAnimationDynamicEvent TestWidgetAnimationEvent;
+
+	// ------------------------ Variable -----------------------
+
+	// ------------------------ Function -----------------------
+
+	virtual void NativeConstruct() override;
+
+	UFUNCTION()
+	void PlayNextAnimation();
+
+	// ------------------------ Function -----------------------
+
+};
