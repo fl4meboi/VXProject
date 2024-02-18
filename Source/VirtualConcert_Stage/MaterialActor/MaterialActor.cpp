96109f7a648f5ec5bf5fe1d3ead3cccda3dc5900
Mumyung <eoalsrud@naver.com>
2024-02-19 03:18:21 +0900

    머티리얼 렌더 타깃을 이용한 머티리얼 텍스처화

---
+++ Source/VirtualConcert_Stage/MaterialActor/MaterialActor.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MaterialActor/MaterialActor.h"
+#include "../ActorComponent/MaterialComponent.h"
+
+// Sets default values
+AMaterialActor::AMaterialActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComp);
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> TempMesh(TEXT("/Script/Engine.StaticMesh'/Engine/BasicShapes/Plane.Plane'"));
+
+	if(TempMesh.Succeeded())
+	{
+		MeshComp->SetStaticMesh(TempMesh.Object);
+	}
+
+	MeshComp->SetCollisionProfileName(FName("ReactObject"));
+
+	
+	
+	MatComp = CreateDefaultSubobject<UMaterialComponent>(TEXT("MatComp"));
+}
+
+// Called when the game starts or when spawned
+void AMaterialActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+	UMaterialInterface* SrcMaterial = Cast<UMaterialInterface>(StaticLoadObject(UMaterialInterface::StaticClass(), nullptr, TEXT("/Script/Engine.Material'/Game/__GDH/Metarials/Custom/M_Flower.M_Flower'")));
+
+	if(SrcMaterial)
+	{
+		DynMat = MeshComp->CreateDynamicMaterialInstance(0, SrcMaterial);
+	}
+
+	// 라이프 싸이클로 인해, 컴포넌트에게 액터 정보 넘겨주기
+	MatComp->SetDynMat(DynMat);
+
+}
+
+// Called every frame
+void AMaterialActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/VirtualConcert_Stage/MaterialActor/SplitRenderActor.cpp
@@ -0,0 +1,48 @@
+#include "MaterialActor/SplitRenderActor.h"
+#include "../ActorComponent/MaterialComponent.h"
+ASplitRenderActor::ASplitRenderActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComp);
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> TempMesh(TEXT("/Script/Engine.StaticMesh'/Engine/BasicShapes/Plane.Plane'"));
+
+	if (TempMesh.Succeeded())
+	{
+		MeshComp->SetStaticMesh(TempMesh.Object);
+	}
+
+	MeshComp->SetCollisionProfileName(FName("ReactObject"));
+
+
+	MatComp = CreateDefaultSubobject<UMaterialComponent>(TEXT("MatComp"));
+
+
+}
+
+void ASplitRenderActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+	UMaterialInterface* SrcMaterial = Cast<UMaterialInterface>(StaticLoadObject(UMaterialInterface::StaticClass(), nullptr, TEXT("/Script/Engine.Material'/Game/__GDH/Metarials/Custom/RenderTarget/Split/M_SplitFlower.M_SplitFlower'")));
+
+	if (SrcMaterial)
+	{
+		DynMat = MeshComp->CreateDynamicMaterialInstance(0, SrcMaterial);
+	}
+
+	// 라이프 싸이클로 인해, 컴포넌트에게 액터 정보 넘겨주기
+	MatComp->SetDynMat(DynMat);
+
+}
+
+void ASplitRenderActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
